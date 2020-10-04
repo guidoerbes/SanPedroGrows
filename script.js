d05afd3ae8e18b3af1f58eecf4b1b01e3cbf1b8e
Guido Iván Erbes <38472141+guidoerbes@users.noreply.github.com>
2020-10-03 23:43:27 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('.hamburger-menu').
+addEventListener('click', ()=>{
+    document.querySelector('.container'). classList.toggle
+    ('change');
+})
+//con este codigo se despliega la slidebar del menu//
